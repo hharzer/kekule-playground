8f70e59e7804af5a620096ff5b9880ef43be23be
Aaron Llanos <allanos@inmotionsoftware.com>
2018-01-15 20:25:26 -0600

    remove node modules from git dir

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const customButton = {
   text: 'Custom',
   htmlClass: 'K-Res-Button-YesOk',
   showText: false,
-  '#execute': function () {
-    const mol = new Kekule.Molecule()
-    const carbon = new Kekule.Atom().setSymbol('C').setCoord2D({x: 0, y: 0})
-    mol.appendNode(carbon)
-    chemComposer.setChemObj(mol)
-  }
+  '#activate': function (args) {
+    console.log('activate', args);
+  },
+  // '#execute': function () {
+  //   console.log('execute');
+  //   // const mol = new Kekule.Molecule()
+  //   // const carbon = new Kekule.Atom().setSymbol('C').setCoord2D({x: 0, y: 0})
+  //   // mol.appendNode(carbon)
+  //   // chemComposer.setChemObj(mol)
+  // },
+  // '#deactivate': function () {
+  //   console.log('deactivate');
+  // },
 }
 
 chemComposer.setDimension('600px', '600px')
 chemComposer.setEnableStyleToolbar(true)
   .setEnableOperHistory(true)
-  .setEnableLoadNewFile(true)
   .setAllowCreateNewChild(false) //multiple structures will be false untill we are ready to add that feature
-  .setCommonToolButtons(['undo', 'redo', 'copy', 'cut', 'paste',
-    'zoomIn', 'reset', 'zoomOut'])   // create all default common tool buttons
-  .setChemToolButtons(['manipulate', 'erase', 'bond', 'atom', 'ring', 'charge', customButton])   // create only chem tool buttons related to molecule
+  .setCommonToolButtons(['undo', 'redo'])
+  .setChemToolButtons(['manipulate', 'bond', 'atom', 'charge', customButton])   // create only chem tool buttons related to molecule
   .setStyleToolComponentNames(['color']);  // create all default style components*/
