a5cbad889d6a494b8ce1ca699823713345ecf1e2
Rachel Agner <rachel.agner@macmillan.com>
2018-01-19 16:55:44 -0600

    playing with css and custom buttons

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const chemComposer = new Kekule.Editor.Composer(document.getElementById('kekule'))
 
-const customButton = {
-  text: 'Custom',
-  htmlClass: 'K-Res-Button-YesOk',
-  showText: false,
+const drawButton = {
+  text: 'Draw',
+  showText: true,
+  '#execute': function () {
+    const mol = new Kekule.Molecule()
+    const carbon = new Kekule.Atom().setSymbol('C').setCoord2D({x: 0, y: 0})
+    mol.appendNode(carbon)
+    chemComposer.setChemObj(mol)
+  }
+}
+
+const editButton = {
+  text: 'Edit',
+  showText: true,
   '#execute': function () {
     const mol = new Kekule.Molecule()
     const carbon = new Kekule.Atom().setSymbol('C').setCoord2D({x: 0, y: 0})
@@ -17,7 +27,16 @@ chemComposer.setEnableStyleToolbar(true)
   .setEnableOperHistory(true)
   .setEnableLoadNewFile(true)
   .setAllowCreateNewChild(false) //multiple structures will be false untill we are ready to add that feature
-  .setCommonToolButtons(['undo', 'redo', 'copy', 'cut', 'paste',
-    'zoomIn', 'reset', 'zoomOut'])   // create all default common tool buttons
-  .setChemToolButtons(['manipulate', 'erase', 'bond', 'atom', 'ring', 'charge', customButton])   // create only chem tool buttons related to molecule
-  .setStyleToolComponentNames(['color']);  // create all default style components*/
+  .setCommonToolButtons(['zoomIn', 'zoomOut']) // create all default common tool buttons
+  .setChemToolButtons(['bond', 'atom', 'manipulate', 'erase', 'charge', 'ring'])   // create only chem tool buttons related to molecule
+  .setStyleToolComponentNames([]);  // create all default style components*/
+
+
+  let renderConfigs = chemComposer.getRenderConfigs()
+      renderConfigs.getLengthConfigs().setBondLineWidth(2)
+      renderConfigs.getLengthConfigs().setAtomFontSize(18)
+      renderConfigs.getLengthConfigs().setDefBondLength(56)
+      renderConfigs.getColorConfigs().setBondColor('#A2D6DD')
+      renderConfigs.getColorConfigs().setAtomColor('#00758E')
+
+  chemComposer.getEditor().repaint()
